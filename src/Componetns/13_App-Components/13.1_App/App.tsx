7a683b6745fac14d1e914197898d2912a30fcb6c
ILLIA BULGAKOV <illia121291@gmail.com>
2024-05-15 11:10:54 +0300

    Crate Food Station

---
+++ src/Componetns/13_App-Components/13.1_App/App.tsx
@@ -0,0 +1,15 @@
+import './App.scss';
+
+import { FC } from 'react';
+
+import HomePage from '../../15_Pages/15.1_Home-Page/15.1.1_HomePage/HomePage';
+
+const App: FC = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Componetns/15_Pages/15.1_Home-Page/15.1.1_HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+import './HomePage.scss';
+
+import { FC } from 'react';
+
+const HomePage: FC = () => {
+  return <div>Home Page</div>;
+};
+
+export default HomePage;
