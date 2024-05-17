02d8f61a74d0a21a87eb70f86d5cbe3d562f34f0
ILLIA BULGAKOV <illia121291@gmail.com>
2024-05-17 07:16:33 +0300

    Added DeliveryLocation Component

--- src/Componetns/13_App-Components/13.2_AppHeader/13.2.8_LogoDeilvery/13.2.8.2_DeliveryLocation/DeliveryLocation.tsx
+++ src/Componetns/13_App-Components/13.2_AppHeader/13.2.8_LogoDeilvery/13.2.8.2_DeliveryLocation/DeliveryLocation.tsx
@@ -1,11 +1,37 @@
 import './DeliveryLocation.scss';
 
-import { FC } from 'react';
+import { FC, useRef, useState } from 'react';
 
 const DeliveryLocation: FC = () => {
+  const [locationState, setLocationState] = useState<boolean>(false);
+  const refUl = useRef<HTMLUListElement>(null);
+
+  const onCllickLocation = (): void => {
+    setLocationState(!locationState);
+  };
+
   return (
     <div className="location__container">
-      <button className="location__btn">Москва ﹀</button>
+      <button className="location__btn" onClick={onCllickLocation}>
+        Москва ﹀
+      </button>
+      <div className={`location__menu `} style={locationState ? { height: refUl.current!.scrollHeight } : { height: '0px' }}>
+        <ul ref={refUl} className="location__list">
+          <li>Area 1</li>
+          <li>Area 2</li>
+          <li>Area 3</li>
+          <li>Area 4</li>
+          <li>Area 5</li>
+          <li>Area 6</li>
+          <li>Area 6</li>
+          <li>Area 6</li>
+          <li>Area 6</li>
+          <li>Area 6</li>
+          <li>Area 6</li>
+          <li>Area 6</li>
+          <li>Area 6</li>
+        </ul>
+      </div>
     </div>
   );
 };
