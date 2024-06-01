29e4fa54463e488b082fbbad9d7d59fc283437a5
ILLIA BULGAKOV <illia121291@gmail.com>
2024-06-01 16:38:45 +0300

    window.scrollTo(0,0) when entering to the cart

--- src/Componetns/13_App-Components/13.2_AppHeader/13.2.7_HeaderAppCart/HeaderAppCart.tsx
+++ src/Componetns/13_App-Components/13.2_AppHeader/13.2.7_HeaderAppCart/HeaderAppCart.tsx
@@ -13,6 +13,7 @@ const HeaderAppCart: FC = () => {
   const currency = useSelector<IStateStore, string>(state => state.currencyPanel.currencyApp);
   const orderList = useSelector<IStateStore, IOrderItem[]>(state => state.cart.orderList);
 
+  // Cart Cost & Quantity -----------------
   let quantity: number = 0;
   let cost: number = 0;
 
@@ -27,7 +28,9 @@ const HeaderAppCart: FC = () => {
   }
 
   return (
-    <NavLink end to="/cart" className="wt150 f_ac pos_rel cart__container">
+    <NavLink end to="/cart" className="wt150 f_ac pos_rel cart__container" 
+    onClick={() => window.scrollTo(0,0)}
+    >
       <span className="f_jc-ac wt25 ht25 bdr50 bkgr__ora fs16 wc cart__qty">{quantity}</span>
       <TiShoppingCart size={45} />
       <span className="fw600">{useDisplayPriceInCurrency(currency, cost)}</span>
